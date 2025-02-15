8be218e78ae4396cb33652c2c5b37aedbe6f6365
Aucki6144 <aucki6144@163.com>
2025-02-15 17:14:25 +0800

    feature: mysql linkage

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -76,9 +76,21 @@ WSGI_APPLICATION = "djangoProject.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'search_engine',
+        'USER': 'django',
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
     }
 }
 
