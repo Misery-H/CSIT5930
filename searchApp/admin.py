6f9b0198a4c265419e12e54b833755ea893f9be9
Aucki6144 <aucki6144@163.com>
2025-02-18 16:26:37 +0800

    feature: mysql model definition

--- searchApp/admin.py
+++ searchApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Document, Term, InvertedIndex
+
+admin.site.register(Document)
+admin.site.register(Term)
+admin.site.register(InvertedIndex)
--- searchApp/models.py
+++ searchApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    url = models.URLField(max_length=512)
+    content_hash = models.CharField(max_length=768, unique=True)  # Use SHA-256 to identify a document
+    title = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    content = models.TextField()
+    crawl_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['content_hash']),
+            models.Index(fields=['crawl_time']),
+        ]
+
+
+class Term(models.Model):
+    term = models.CharField(max_length=255, unique=True)
+    df = models.IntegerField(default=1)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['term']),
+        ]
+
+
+class InvertedIndex(models.Model):
+    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    tf_max = models.IntegerField(default=1)
+    tf = models.IntegerField(default=1)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['term']),
+        ]
