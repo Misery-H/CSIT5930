35ced16b7f33dc897f703ac06fe916aefdacc113
Aucki6144 <aucki6144@163.com>
2025-02-27 19:05:47 +0800

    fix: app name lowercase

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'searchApp.apps.LowercaseTableNameAppConfig',
+    'searchApp.apps.SearchappConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- searchApp/apps.py
+++ searchApp/apps.py
@@ -5,15 +5,9 @@ class SearchappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'searchApp'
 
-
-class LowercaseTableNameAppConfig(AppConfig):
-    name = 'searchApp'
-
     def ready(self):
-        from django.db.models.signals import class_prepared
+        from django.apps import apps
 
-        def make_lowercase(sender, **kwargs):
-            if sender._meta.app_label == self.label:
-                sender._meta.db_table = sender._meta.db_table.lower()
-
-        class_prepared.connect(make_lowercase)
+        for model in apps.get_models():
+            if not model._meta.db_table:
+                model._meta.db_table = model._meta.model_name.lower()
